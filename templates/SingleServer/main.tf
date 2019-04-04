f8aae50ecbace38f1c2dbb09ba29a8723c8047da
jfoster <jfoster@ztech.io>
2019-04-04 17:37:32 +0000

    changed resource

--- templates/SingleServer/main.tf
+++ templates/SingleServer/main.tf
@@ -15,20 +15,23 @@ provider "ibm" {
 }
 
 
-resource "ibm_compute_vm_instance" "single_vm" {
-  cores       = 8
-  memory      = 16384
-  domain      = "${var.single_vm_domain}"
-  hostname    = "${var.single_vm_hostname}"
-  datacenter  = "${var.single_vm_datacenter}"
-  ssh_key_ids = ["${ibm_compute_ssh_key.auth.id}"]
-  os_reference_code = "${var.single_vm_os_reference_code}"
-  hourly_billing = true
-  local_disk = "${var.local_disk}"
-  disks = [100,350]
-  private_security_group_ids = ["${var.ICP_private_name}"]
-  public_security_group_ids = ["${var.ICP_public_name}"]
+resource "ibm_compute_vm_instance" "cam-mcm" {
+  hostname             = "cam-mcm"
+  domain               = "ztechsl.io"
+  os_reference_code    = "UBUNTU_16_64"
+  datacenter           = "dal13"
+  network_speed        = 100
+  hourly_billing       = true
+  private_network_only = false
+  cores                = 8
+  memory               = 16384
+  disks                = [100,350]
+  local_disk           = false
+  private_security_group_ids = [1117823]
+  public_security_group_ids = [1117813]
+  ssh_key_ids          = [1134553,1345699]
 }
+
 
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
--- templates/SingleServer/variables.tf
+++ templates/SingleServer/variables.tf
@@ -15,43 +15,7 @@ variable "ibm_sl_api_key" {
   description = "Generated"
 }
 
-variable "single_vm_domain" {
-  type = "string"
-  description = "The domain for the computing instance."
-}
-
-variable "single_vm_hostname" {
-  type = "string"
-  description = "The hostname for the computing instance."
-}
-
-variable "single_vm_datacenter" {
-  type = "string"
-  description = "The datacenter in which you want to provision the instance. NOTE: If dedicated_host_name or dedicated_host_id is provided then the datacenter should be same as the dedicated host datacenter."
-}
-
-variable "single_vm_os_reference_code" {
-  type = "string"
-  description = "Generated"
-}
-
 variable "ibm_ssh_key_name" {
-  type = "list"
-  description = "Generated"
-}
-
-variable "local_disk" {
-  type = "string"
-  description = "Generated"
-  default = "false"
-}
-
-variable "ICP_private_name" {
-  type = "list"
-  description = "Generated"
-}
-
-variable "ICP_public_name" {
   type = "list"
   description = "Generated"
 }
