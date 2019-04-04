1bfed170c006db4e163d1c37eeb0b05773113509
jfoster <jfoster@ztech.io>
2019-04-04 16:06:47 +0000

    updated disk sizes

--- templates/SingleServer/main.tf
+++ templates/SingleServer/main.tf
@@ -29,10 +29,7 @@ resource "ibm_compute_vm_instance" "single_vm" {
   private_vlan_id       = "${var.ibm_network_private_vlan_id}"
   hourly_billing = true
   local_disk = false
-  disks = [
-    "${var.["disk_size"]}",
-    "${var.["docker_vol_size"]}"
-  ]
+  disks = [100,350]
   private_security_group_ids = ["${var.ICP_private_private_id}"]
   public_security_group_ids = ["${var.ICP_public_public_id}"]
 }
